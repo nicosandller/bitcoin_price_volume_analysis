cce3d5e3af4716e4133790ffeb5041d6bc7fe2b7
Nicolas Sandller <nicolassandller@Nicolass-MacBook-Pro.local>
2016-10-29 10:16:25 -0300

    adds connection error message when status_code is not 200. Functions return list of prices and tx_volume instead of dicts with unix timestamp

--- run.py
+++ run.py
@@ -4,14 +4,25 @@ import requests
 def get_prices():
     url = 'https://api.blockchain.info/charts/market-price?' + \
           'format=json&timespan=all'
-    r = requests.get.requests(url)
-    prices = r.json()
-    return prices
+    r = requests.get(url)
+    if r.status_code != 200:
+        print 'Connection error'
+        return False
+    prices = [datapoint['y'] for datapoint in r.json()['values']]
+    dates = [datapoint['y'] for datapoint in r.json()['values']]
+    return prices, dates
 
 
-def get_tx_volume():
+def get_tx_volumes():
     url = 'https://api.blockchain.info/charts/n-transactions-' + \
           'excluding-popular?format=json&timespan=all'
-    r = requests.get.requests(url)
-    tx_volume = r.json()
-    return tx_volume
+    r = requests.get(url)
+    if r.status_code != 200:
+        print 'Connection error'
+        return False
+    tx_volumes = [datapoint['y'] for datapoint in r.json()['values']]
+    return tx_volumes
+
+
+def graph_chart(prices, tx_volumes):
+    return
