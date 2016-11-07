c22c777e7b02a15ed6ba70335b07618e3f360995
Nicolas Sandller <nicolassandller@Nicolass-MacBook-Pro.local>
2016-11-07 13:34:40 -0300

    adds support for arguements (all / 1month / 2weeks / 5days)

--- run.py
+++ run.py
@@ -1,31 +1,37 @@
 import requests
+import sys
 import matplotlib.pyplot as plt
 from datetime import datetime
 from scipy import interpolate
 
 SMOOTH_FACTOR = 150000
+TIMESPAN = sys.argv[1]
 
 
-def get_prices():
+def get_prices(timespan='all'):
     url = 'https://api.blockchain.info/charts/market-price?' + \
-          'format=json&timespan=all'
+          'format=json&timespan=' + timespan
     r = requests.get(url)
     if r.status_code != 200:
         print 'Connection error'
         return False
+    if 'error' in r.json():
+        print r.json()['error']
     prices = [datapoint['y'] for datapoint in r.json()['values']]
     dates = [datetime.fromtimestamp(datapoint['x'])
              for datapoint in r.json()['values']]
     return prices, dates
 
 
-def get_tx_volumes(smooth_factor):
+def get_tx_volumes(smooth_factor, timespan='all'):
     url = 'https://api.blockchain.info/charts/n-transactions-' + \
-          'excluding-popular?format=json&timespan=all'
+          'excluding-popular?format=json&timespan=' + timespan
     r = requests.get(url)
     if r.status_code != 200:
         print 'Connection error'
         return False
+    if 'error' in r.json():
+        print r.json()['error']
     tx_volumes = [datapoint['y'] / 1000 for datapoint in r.json()['values']]
     x = [datapoint['x'] for datapoint in r.json()['values']]
     tck = interpolate.splrep(x, tx_volumes, k=3, s=smooth_factor)
@@ -50,7 +56,12 @@ def graph_chart(prices, tx_volumes, dates):
     plt.show()
     return True
 
-prices, dates = get_prices()
-tx_volumes, transformed = get_tx_volumes(SMOOTH_FACTOR)
+prices, dates = get_prices(TIMESPAN)
+tx_volumes, transformed = get_tx_volumes(SMOOTH_FACTOR, TIMESPAN)
 # graph_chart(prices, tx_volumes, dates)
-graph_chart(prices, transformed, dates)
+if prices is not False:
+    if TIMESPAN == 'all':
+        print 'transformed series'
+        graph_chart(prices, transformed, dates)
+    else:
+        graph_chart(prices, tx_volumes, dates)
